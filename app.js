4c9571c64336da24a8e23f1b388e6e46d59bf3c3
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2020-12-02 15:21:05 +0200

    https for localhost and probably server

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const passport = require('./Utils/pass');
 const rootRoute = require('./Routes/rootRoute');
 const picRoute = require('./Routes/picRoute');
@@ -10,11 +9,8 @@ const userRoute = require('./Routes/userRoute');
 const authRoute = require('./Routes/authRoute');
 const likeRoute = require('./Routes/likeRoute');
 const commentRoute = require('./Routes/commentRoute');
-
 const app = express();
-const port = 3000;
 
-app.use(cookieParser());
 
 app.use(cors());
 
@@ -31,12 +27,20 @@ app.use(express.static('.'));
 //Serve static files from thumbnails.
 app.use('/Thumbnails', express.static('Thumbnails'));
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'production') {
+  require('./production')(app, process.env.PORT);
+} else {
+  require('./localhost')(app, process.env.HTTPS_PORT, process.env.HTTP_PORT);
+}
+
 app.use('/', rootRoute);
 app.use('/auth', authRoute);
 app.use('/pic', passport.authenticate('jwt', {session: false}), picRoute);
 app.use('/user', passport.authenticate('jwt', {session: false}), userRoute);
 app.use('/likes', passport.authenticate('jwt', {session: false}), likeRoute);
-app.use('/comments', passport.authenticate('jwt', {session: false}), commentRoute);
+app.use('/comments', passport.authenticate('jwt', {session: false}),
+    commentRoute);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
