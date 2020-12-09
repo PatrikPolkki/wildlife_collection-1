009eecb078cc9cc5d83c8b1f7bdd3638a4fc18db
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2020-12-09 17:53:12 +0200

    deployable version

--- Routes/rootRoute.js
+++ Routes/rootRoute.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   console.log('rootRoute: root route with req:', req.query);
   //res.send(`Hello world <a href="cat">Click</a><br>
   //with test is ${req.query.test} and fsun is ${req.query.more}`);
-  //res.sendFile('index.html', { root: '.' });
+  //res.sendFile('test.html', { root: '.' });
   //res.send(`<h1>Hello Index page</h1>`);
   res.render('index.html')
 });
